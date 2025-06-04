b989144d4f129096decca7c4b41f7bdd75dcaf24
Anton Shtern <antsht@gmail.com>
2025-06-04 21:36:13 +0300

    Add: Base View templates from design

---
+++ MyCompany/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCompany.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- MyCompany/Program.cs
+++ MyCompany/Program.cs
@@ -1,14 +1,41 @@
+﻿using MyCompany.Infrastructure;
+
 namespace MyCompany;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-        app.MapGet("/", () => "Hello World!");
+        // подключаем в конфигурацию файл appsettings.json
+        IConfigurationBuilder configBuild = new ConfigurationBuilder()
+            .SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
 
-        app.Run();
+        // оборачиваем секцию Project в форму объекта для удобства
+        IConfiguration configuration = configBuild.Build();
+        AppConfig config = configuration.GetSection("Project").Get<AppConfig>() 
+            ?? throw new FileNotFoundException("Can't load config from appsettings.json"); 
+
+        // подключаем функционал контроллеров
+        builder.Services.AddControllersWithViews();
+
+        // Собираем конфигурацию
+        WebApplication app = builder.Build();
+
+        // ! Порядок следования middleware очень важен !
+
+        // Подключаем использование статических файлов
+        app.UseStaticFiles();
+
+        // подключаем систему маршрутизации
+        app.UseRouting();
+
+        // регистрируем маршрут
+        app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+        await app.RunAsync();
     }
 }
